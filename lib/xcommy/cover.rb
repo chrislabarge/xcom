f38c0b1f81a4cd4f05cf3d9f3dc90c27993a2701
Chris LaBarge <chrislabargedev@gmail.com>
2022-02-28 14:21:00 -0500

    have Cover inherit from Entity

--- lib/xcommy/cover.rb
+++ lib/xcommy/cover.rb
@@ -1,12 +1,18 @@
 module Xcommy
-  class Cover
-    attr_accessor :position
+  class Cover < Entity
     attr_reader :type
 
-    def initialize(game, spot, type)
-      @game = game
-      @position = spot
+    def initialize(game, starting_position, type)
+      super(game, starting_position)
       @type = type.to_sym
+    end
+
+    def position=(value)
+      @current_position = value
+    end
+
+    def position
+      @current_position
     end
 
     def in_between?(spot1, spot2)
@@ -20,13 +26,13 @@ module Xcommy
 
     def inline_with?(spot1, spot2, alignment_type)
       index = (alignment_type.to_sym == :vertically ? 1 : 0)
-      position[index] == spot1[index] && position[index] == spot2[index]
+      @current_position[index] == spot1[index] && @current_position[index] == spot2[index]
     end
 
     def between?(spot1, spot2, alignment_type)
       index = (alignment_type.to_sym == :vertically ? 0 : 1)
-      (spot1[index] < position[index] && spot2[index] > position[index] ||
-         spot1[index] > position[index] && spot2[index] < position[index])
+      (spot1[index] < @current_position[index] && spot2[index] > @current_position[index] ||
+         spot1[index] > @current_position[index] && spot2[index] < @current_position[index])
     end
   end
 end
